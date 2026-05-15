53dfca55653281b88e25e624db5ceaea60c48d78
Kevin Wang <demonassed@gmail.com>
2026-05-15 18:48:59 +0800

    Fix nodes/__init__.py - remove rag_qa_node, add test_executor_node

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -3,6 +3,7 @@ from .chat_node import chat_node
 from .planner_node import planner_node
 from .designer_node import designer_node
 from .code_analysis_node import code_analysis_node, ask_project_path_node
-from .rag_qa_node import rag_qa_node
+from .rag_retrieve_node import rag_retrieve_node
+from .test_executor_node import test_executor_node, ask_test_path_node
 
-__all__ = ["intent_node", "chat_node", "planner_node", "designer_node", "code_analysis_node", "ask_project_path_node", "rag_qa_node"]
+__all__ = ["intent_node", "chat_node", "planner_node", "designer_node", "code_analysis_node", "ask_project_path_node", "rag_retrieve_node", "test_executor_node", "ask_test_path_node"]
